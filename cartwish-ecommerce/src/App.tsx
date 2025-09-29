41c58a3e4772ef88c1b599cf1e0310e4bc6bb8f0
Borna Miljković <borna.milja@gmail.com>
2025-09-29 12:56:33 +0200

    singleProductpage i search button u index

--- cartwish-ecommerce/src/App.tsx
+++ cartwish-ecommerce/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 // import HomePage from "./components/Home/HomePage";
 import ProductsPage from "./components/Products/ProductsPage";
+import SingleProductPage from "./components/SingleProduct/SingleProductPage";
 const App = () => {
   return (
     <div className="app">
@@ -9,6 +10,7 @@ const App = () => {
       <main>
         {/* <HomePage /> */}
         <ProductsPage />
+        <SingleProductPage />
       </main>
     </div>
   );
