4a1e4b0df0de84b886289e5ccd5ba5ebb45de179
Borna Miljković <borna.milja@gmail.com>
2025-10-24 01:02:25 +0200

    form validation signup sa zodom i form hook

--- cartwish-ecommerce/src/App.tsx
+++ cartwish-ecommerce/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import SignupPage from "./components/Authentication/SignupPage";
 // import HomePage from "./components/Home/HomePage";
 // import ProductsPage from "./components/Products/ProductsPage";
 // import SingleProductPage from "./components/SingleProduct/SingleProductPage";
 // import CartPage from "./components/Cart/CartPage";
-import LoginPage from "./components/Authentication/LoginPage";
+// import LoginPage from "./components/Authentication/LoginPage";
 const App = () => {
   return (
     <div className="app">
@@ -15,7 +16,8 @@ const App = () => {
         {/* <SingleProductPage /> */}
         {/* <CartPage /> */}
         {/* <MyOrderPage /> */}
-        <LoginPage />
+        {/* <LoginPage /> */}
+        <SignupPage />
       </main>
     </div>
   );
