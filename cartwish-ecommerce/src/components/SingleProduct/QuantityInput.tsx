239edad0add02c7edbdc7c38146b853518f0565d
Borna Miljković <borna.milja@gmail.com>
2025-11-09 23:48:28 +0100

    comment

--- cartwish-ecommerce/src/components/SingleProduct/QuantityInput.tsx
+++ cartwish-ecommerce/src/components/SingleProduct/QuantityInput.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./QuantityInput.css";
 // rodendan jbg nemam vremena gulas i planinarenje
+// nikin tata rucak gabrek,repete gulasa,druzenje sa starcima i dorom i njenim deckom
 const QuantityInput = ({quantity, setQuantity ,stock}) => {
   const [count, setCount] = useState(1);
   return (
