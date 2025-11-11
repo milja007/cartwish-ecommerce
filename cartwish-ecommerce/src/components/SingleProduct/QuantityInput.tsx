b383ab6cbd551d0f801febe10cd5ae5a1e411ca7
Borna Miljković <borna.milja@gmail.com>
2025-11-11 23:29:04 +0100

    comment

--- cartwish-ecommerce/src/components/SingleProduct/QuantityInput.tsx
+++ cartwish-ecommerce/src/components/SingleProduct/QuantityInput.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./QuantityInput.css";
 // Zagreb intid doo fiskalizacija za aparate i ostali poslovi evo doma 22:35 vrijeme za spavanac sutra jako ili nikako
+// obavljao sastanke i ponude za hugos loyalty progr i realestate i trening zakazao opet sutra cijeli dan na kompu 
 const QuantityInput = ({quantity, setQuantity ,stock}) => {
   const [count, setCount] = useState(1);
   return (
